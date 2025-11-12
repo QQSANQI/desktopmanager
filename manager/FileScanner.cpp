9b1bcc0981706cfeba3de1561d3e378ccf7e559a
mailei <2503326813@qq.com>
2025-11-12 21:26:41 +0800

    mario

--- manager/FileScanner.cpp
+++ manager/FileScanner.cpp
@@ -1,18 +1,44 @@
 #include "FileScanner.h"
 using namespace std;
-
-fs::path FileScanner::getALLFiles()//遍历所有文件
+/*假设传入的路径正确
+我不管，必须正确
+*/
+std::vector<fs::path> FileScanner::getAllFiles(const fs::path& desktopPath)//遍历所有文件，返回文件路径列表
 {
-    fs::path desktopPath;
-	cout << "目录内容" << endl;
-    for (const auto& entry : desktopPath)
-    {
-		cout << entry.path() << endl;
-    }
-    return fs::path();
+	vector<fs::path> allFiles;
+	//遍历desktopPath目录下的所有文件和文件夹
+	if (!fs::exists(desktopPath))//路径不存在，报错
+		{
+			cout << "路径不存在" << endl;
+			return std::vector<fs::path>();
+		}
+	else 
+		{
+		for (const auto& entry : fs::directory_iterator(fs::absolute(desktopPath)))
+			{
+				allFiles.push_back(entry.path());
+			}
+		return allFiles;
+		}
 }
 
-fs::path FileScanner::getFilesByType()//分类获取文件
+std::vector<fs::path> FileScanner::getFilesByType(const std::string& extension)//分类获取文件，返回指定类型的文件路径列表
 {
-    return fs::path();
+	vector<fs::path> filesByType;
+	try {
+		for (fs::path files : getAllFiles(desktopPath)) {
+			if(files.extension() == extension)
+			{
+				filesByType.push_back(files);
+			}
+		}
+		if(filesByType.size()==0)
+		{
+			throw runtime_error("未找到该类型文件");
+		}
+	}
+	catch (const runtime_error& e) {
+		cerr << "错误: " << e.what() << endl;
+	}//未找到就报错，此时返回空数组
+	return filesByType;
 }
--- manager/FileScanner.h
+++ manager/FileScanner.h
@@ -9,10 +9,9 @@ namespace fs = std::filesystem; // 文件系统库
 // 模块1：文件扫描器 - 负责获取桌面文件信息
 class FileScanner {
 public:
-    fs::path getALLFiles();
-    fs::path getFilesByType();
-    std::vector<fs::path> getAllFiles(const fs::path& desktopPath);
-    std::vector<fs::path> getFilesByType(const std::string& extension);
+	fs::path& desktopPath;//桌面路径
+	std::vector<fs::path> getAllFiles(const fs::path& desktopPath);//遍历所有文件，返回文件路径列表
+	std::vector<fs::path> getFilesByType(const std::string& extension);//分类获取文件，返回指定类型的文件路径列表
 private:
-
+   
 };
